cad3b0dec24e23af9cef958370d337023c97c78f
Afaq Ahmed <khatri.afaq@gmail.com>
2024-02-06 16:53:51 +0500

    faqs

--- components/Faqs/FaqsData.ts
+++ components/Faqs/FaqsData.ts
@@ -3,7 +3,7 @@ export const FaqsData = [
     queNo: 1,
     question: "What is MYGOLD Token?",
     answer:
-      // "MYGOLD Token (MYGT) is a gold-backed token that is minted and distributed by precious metals dealers using a blockchain-based solution powered by Artifical Intelligence. Each token represents full ownership and can be redeemed for 1 gram of 99.99% gold, sourced from LBMA-accredited refineries. The underlying gold is stored in fully insured and audited vaulting facilities around the world.",
+      ".",
   },
   {
     queNo: 2,
@@ -19,40 +19,40 @@ export const FaqsData = [
     queNo: 4,
     question: "How can I ensure that MYGOLD Token are 100% backed by gold ?",
     answer:
-      // "The blockchain platform developed by Aurus Technologies Ltd. ensures that MYGOLD (MYGT) tokens are fully backed by gold throughout their entire lifecycle. Firstly, MYGOLD tokens can only be issued once provenance and proof of ownership have been validated by the custodian and an external auditor. Each MYGOLD token is backed by 1 gram of 99.99% LBMA-accredited gold and is traceable to a single bullion and its unique registration number. Vaults are audited regularly by an accredited auditor to ensure that the underlying gold reserves are consistent with the MYGOLD token supply. To review the custody reports of the gold reserves.",
+      ".",
   },
   {
     queNo: 5,
     question: "Where can I Store my MYGOLD Token?",
-    answer: "",
+    answer: ".",
   },
   {
     queNo: 6,
     question: "What is a cryptocurrency wallet?",
-    answer: "",
+    answer: ".",
   },
   {
     queNo: 7,
     question: "What is a MetaMask Wallet?",
     answer:
-      // "MetaMask is a digital wallet that allows you to store Ether and other ERC-20 tokens, such as MYGOLD Token. You can download MetaMask as an extension/plugin for your web browser (Chrome, Firefox, Edge and Brave) and/or as a mobile app (iOS and Android). With MetaMask you will be able to store and manage the MYGOLD tokens, but you will also have the possibility to receive, send and buy other cryptocurrencies.",
+      ".",
   },
   {
     queNo: 8,
     question: "Can I trade MYGT MYGOLD token?",
     answer:
-      // "Yes, users have the flexibility to trade their VRL Virtual X tokens on various crypto exchanges, turning their rewards into a financial asset. ",
+      ".",
   },
   {
     queNo: 9,
     question: "What future services are planned for MYGOLD Token? ",
     answer:
-      // "While specific details are yet to be disclosed, Virtual X plans to introduce a range of innovative features, including potential AI integrations, loyalty programs, financial services, and more, to enhance the user experience. ",
+      ".",
   },
   {
     queNo: 10,
     question: "How can I stay updated on MYGOLD Token developments? ",
     answer:
-      // "Keep an eye on our official website and follow us on our social media channels for the latest updates and announcements regarding MYGOLD Token services and features.",
+      ".",
   },
 ];
